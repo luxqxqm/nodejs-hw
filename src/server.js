8f83a7d6fe41dee06c6d2d59aa7ceab66a50d6d3
Den Yaseniuk <denasenuk@gmail.com>
2026-06-28 13:18:03 +0200

    hw 4

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRouter from './routes/authRoutes.js';
 const app = express();
 
 app.use(logger); //  1. Логер першим — бачить усі запити
@@ -14,11 +15,11 @@ app.use(express.json()); // 2. Парсинг JSON-тіла
 app.use(cors()); // 3. Дозвіл для запитів з інших доменів
 
 app.use(notesRoutes);
+app.use(authRouter);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
 await connectMongoDB();
-
 const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => console.log(`Server work on PORT: ${port}`));
